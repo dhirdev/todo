9f4196a7152432ea9ae6eab40dd7987514e28327
dhirdev <dhirajdev.stuff@gmail.com>
2024-06-01 11:47:10 +0530

    first commit

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('submit-todo', views.submit_todo, name="submit-todo"),
+    path('complete-todo/<int:pk>', views.complete_todo, name="complete-todo"),
+    path('delete-todo/<int:pk>', views.delete_todo, name="delete-todo"),
+
+]
